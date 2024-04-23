6f0c46ad081054e3b60304e6f040c8424915dcea
Anasch113 <anaschaudry2002@gmail.com>
2024-04-23 23:37:50 +0500

    commit april 23 2024

---
+++ problem9.js
@@ -0,0 +1,11 @@
+// Write a function argumentsLength that returns the count of arguments passed to it.
+ 
+
+
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */

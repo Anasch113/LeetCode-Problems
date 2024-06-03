873c5766004aab98a5409128fc0aa29f8c087494
Anasch113 <anaschaudry2002@gmail.com>
2024-06-03 17:16:46 +0500

    Execute asyncronous functions in parallel. No 19

--- problem19.js
+++ problem19.js
@@ -11,3 +11,26 @@
 // Please solve it without using the built-in Promise.all function
 
 
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => { 
+
+
+        let res = Array(functions.length);
+        let waitingFor = functions.length
+
+        for(let i = 0; i < functions.length; i++){
+
+            functions[i]().then((result)=>{
+                res[i] = result;
+                waitingFor--
+                if(waitingFor == 0) resolve(res)
+
+            }).catch(reject)
+        }
+    })
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */

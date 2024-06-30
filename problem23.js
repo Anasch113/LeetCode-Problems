fd1ca8080af912f1c9dcede49719ff4d99c30e63
Anasch113 <anaschaudry2002@gmail.com>
2024-06-30 18:12:49 +0500

    commit

---
+++ problem23.js
@@ -0,0 +1,32 @@
+// 2631. Group By
+
+// Write code that enhances all arrays such that you can call the array.groupBy(fn) method on any array and it will return a grouped version of the array.
+
+// A grouped array is an object where each key is the output of fn(arr[i]) and each value is an array containing all items in the original array with that key.
+
+// The provided callback fn will accept an item in the array and return a string key.
+
+// The order of each value list should be the order the items appear in the array. Any order of keys is acceptable.
+
+// Please solve it without lodash's _.groupBy function.
+
+
+
+
+Array.prototype.groupBy = function (fn) {
+
+    return this.reduce((grouped, item) => {
+
+        const key = fn(item)
+
+        if (!grouped[key]) {
+            grouped[key] = []
+        }
+
+        grouped[key].push(item)
+        return grouped
+    }, {})
+
+};
+
+ 
